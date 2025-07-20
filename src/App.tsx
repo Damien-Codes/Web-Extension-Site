5afbbc67f7f793224225db40cddf117f155fed54
Damien-Codes <pinodamien@gmail.com>
2025-07-20 18:13:11 +0200

    Update route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
@@ -7,16 +8,17 @@ import PrivacyPage from './pages/PrivacyPage';
 import ContactPage from './pages/ContactPage';
 
 function App() {
-  // Get current path to determine which page to show
-  const currentPath = window.location.pathname;
-
   return (
     <div className="App min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900">
-      <Header />
-      {currentPath === '/legal' ? <LegalPage /> : 
-       currentPath === '/privacy' ? <PrivacyPage /> : 
-       currentPath === '/contact' ? <ContactPage /> : 
-       <HomePage />}
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/legal" element={<LegalPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
